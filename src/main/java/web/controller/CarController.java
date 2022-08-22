c0bf1768e96671296058743a5cd12aee13c1782e
abahaan.kokov@gmail.com <pOj65xT69>
2022-08-22 19:27:14 +0700

    Task 2_2_2

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,31 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.services.CarService;
+
+@Controller
+@RequestMapping("/cars")
+public class CarController {
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+
+    @GetMapping()
+    public String getCountCar(@RequestParam(value = "count", required = false) Integer i,
+                              Model model) {
+        if (i == null) {
+            i = 0;
+        }
+        model.addAttribute("cars", carService.listCars(i));
+        return "/cars";
+    }
+}
